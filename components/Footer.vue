0d3700d3a0cb9e4709de485430ba06e004374b4a
Roman-00 <rvasin00@gmail.com>
2021-12-18 12:28:03 +0600

    add a mobile version

---
+++ components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+	<footer id="footer" class="footer">
+		<div class="container">
+			<div class="footer-content">
+				<div class="footer-location">
+					<location />
+					Екатеринбург
+				</div>
+
+				<div class="footer-contacts">
+					<a href="tel:+79221076701" class="footer-contacts__phone">
+						+7 (922) 107-67-01
+					</a>
+					<a href="mailto:ivanservices54@gmail.com" class="footer-contacts__email">
+						ivanservices54@gmail.com
+					</a>
+				</div>
+
+				<p class="footer-copyright">
+					© 2021 iVanService - Сервисный центр.
+					<span>Все права защищены.</span>
+				</p>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+import location from '~/assets/images/map.svg';
+
+export default {
+	name: 'Footer',
+	components: {
+		location,
+	},
+}
+</script>
---
+++ components/PageComponents/Index/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+	<section id="hero" class="hero">
+		<div class="container">
+			<div class="hero-content">
+				<div class="hero-blocks">
+					<div class="hero-block">
+						<img
+							src="~/assets/images/banner-large.jpg"
+							alt="Мы ремонтируем все линейки стиральных машин. А так же Посудомоечные машины*">
+					</div>
+					<div class="hero-block">
+						<img
+							src="~/assets/images/banner-small.jpg"
+							alt="Ремонт Микроволновых печей">
+					</div>
+					<div class="hero-block">
+						<img
+							src="~/assets/images/banner-small2.jpg"
+							alt="Ремонт Телевизтонного оборудования">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Hero'
+}
+</script>
